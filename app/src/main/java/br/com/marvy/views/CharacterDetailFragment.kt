cb914262ef7923434c2e90f4ca61eac5a68ddac8
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2020-09-19 18:45:37 -0300

    feat(app): add navigation logic
    Add navigation logic between list fragment and its detail counterpart

--- app/src/main/java/br/com/marvy/views/CharacterDetailFragment.kt
+++ app/src/main/java/br/com/marvy/views/CharacterDetailFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.com.marvy.R
 
+
 class CharacterDetailFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/br/com/marvy/views/CharactersListFragment.kt
+++ app/src/main/java/br/com/marvy/views/CharactersListFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import br.com.marvy.R
+import kotlinx.android.synthetic.main.fragment_characters_list.*
+
 
 class CharactersListFragment : Fragment() {
 
@@ -14,6 +17,13 @@ class CharactersListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_characters_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        listBtn.setOnClickListener { btn ->
+            btn.findNavController()
+                .navigate(R.id.to_detail)
+        }
     }
 
     companion object {
